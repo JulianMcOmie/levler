7d9fa4d7bc7642cc8831cccb1a106cf97a85177e
Julian McOmie <julian.mcomie@gmail.com>
2025-11-11 23:56:22 -0800

    remove click taking back to input

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
   };
 
   const handleDisplayClick = () => {
-    setShowInput(true);
-    setInputValue('');
-    setDisplayText('');
+    // setShowInput(true);
+    // setInputValue('');
+    // setDisplayText('');
   };
 
   return (
